7fb6682b1ddb8d61b2c7674fcc9c6d78750c6bf2
Trevor Barnes <tbarnes@blueorigin.com>
2025-06-04 16:52:12 -0700

    initial push

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+
+mod rss_puller;
+use rss_puller::extractor::RssClient;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    
+    let rss = RssClient::new("https://spacenews.com/feed/");
+    println!("{rss:#?}");
+    /*
+    //let url = "https://www.darpa.mil/rss.xml";
+    let url = "https://spacenews.com/feed/";
+    let feed = read_rss(url).await?;
+    
+    let link = read_link_from_rss(feed).await?;
+    
+    println!("{link:#?}");
+    
+    let bodies = get_content_from_feed(link).await?;
+    
+    let resp = join_content(bodies).await?;
+    
+    resp.iter()
+        .for_each(|article| println!("{:#?}", article));
+    
+    //let mds = html_to_md(resp).await?;
+    
+        //mds.iter()
+        //.for_each(|article| println!("{:#?}", article));
+    */
+    Ok(())
+}
---
+++ src/rss_puller/extractor.rs
@@ -0,0 +1,116 @@
+use tokio::task::JoinHandle;
+
+use anyhow::Result;
+use rss::Channel;
+use soup::prelude::*;
+
+pub struct RssClient<'a> {
+    pub url: &'a str,
+    pub channel: Channel,
+}
+
+#[derive(Debug)]
+impl <'a>RssClient<'a> {
+    pub async fn new(url: &'a str) -> Self {
+        Self {
+            url,
+            channel: read_rss(url).await.unwrap(),
+            links: read_links_from_rss(channel).await.unwrap(),
+            html_response: join_content_into_html(get_content_from_feed(links)).await.unwrap(),
+            md_response: html_to_md(html_response),
+        }
+    }
+}
+
+async fn read_rss(url: &str) -> Result<Channel> {
+    // Using reqwest, read the bytes from the rss channel
+    let content = reqwest::get(url)
+        .await?
+        .bytes()
+        .await?;
+    
+    // Write to the channel Struct
+    let channel = Channel::read_from(&content[..])?;
+    
+    Ok(channel)
+}
+
+
+async fn read_link_from_rss(channel: Channel) -> Result<Vec<String>> {
+    // Get links form channel
+    let mut links: Vec<String> = vec![];
+    channel.items.iter()
+        .for_each(|item| links.push(item.link().unwrap().to_string()));
+    
+    
+    Ok(links)
+}
+
+async fn get_content_from_feed(links: Vec<String>) -> Result<Vec<String>> {
+    let mut join_handle: Vec<JoinHandle<String>> = vec![];
+    
+    let mut bodies: Vec<String> = vec![];
+    
+    for link in links {
+        let handle = tokio::spawn(async move{
+            let body = reqwest::get(link.clone())
+                .await.unwrap()
+                .text()
+                .await.unwrap();
+            
+            body
+        });
+        
+        join_handle.push(handle);
+    }
+    
+    for handle in join_handle {
+        bodies.push(handle.await?);
+    }
+    
+    Ok(bodies)
+}
+
+async fn join_content_into_html(content: Vec<String>) -> Result<Vec<String>> {
+    let mut join_handle: Vec<JoinHandle<String>> = vec![];
+    
+    let mut resp: Vec<String> = vec![];
+    
+    for c in content {
+        let handle = tokio::spawn(async move {
+            let soup = Soup::new(c.as_str());
+            
+            let ap_all = soup.tag(true)
+                .find_all()
+                .filter(|tag| tag.name() == "article");
+            
+            let body = ap_all
+                .map(|p| p.display())
+                .collect::<Vec<_>>()
+                .join("\n");
+            
+            body
+        });  
+        
+        join_handle.push(handle);
+    }
+    
+    for handle in join_handle {
+        resp.push(handle.await?);
+    }
+    
+    Ok(resp)
+}
+
+async fn html_to_md(html: Vec<String>) -> Result<Vec<String>> {
+    let mut mds: Vec<String> = vec![];
+    
+    for h in html {
+        let parsed = html2md::parse_html(&h);
+        let clean = parsed.replace("\n\n", "");
+        
+        mds.push(clean);
+    }
+    
+    Ok(mds)
+}
